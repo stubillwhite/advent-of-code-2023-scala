fefe76783d218d13610cb496ef01c8efb2c8b533
Stu W <stubillwhite@gmail.com>
2023-12-02 12:39:48 +0000

    Solution to day one part two

--- src/main/scala/adventofcode/Day01.scala
+++ src/main/scala/adventofcode/Day01.scala
@@ -9,23 +9,64 @@ object Day01 {
 
   def solutionPartOne(input: String): Int = {
     parseInput(input)
-      .map(extractCalibrationValues)
+      .map(extractDigits(numeralDigitsExtractor))
+      .map(calibrationValue)
+      .sum
+  }
+
+  // Part two
+
+  def solutionPartTwo(input: String): Int = {
+    parseInput(input)
+      .map(extractDigits(numeralAndWordDigitsExtractor))
       .map(calibrationValue)
       .sum
   }
 
   def main(args: Array[String]): Unit = {
     println(s"Part one: ${solutionPartOne(problemInput)}")
+    println(s"Part two: ${solutionPartTwo(problemInput)}")
     println("Done")
   }
+
+  private type DigitsExtractor = String => Seq[String]
+
+  private val numeralDigitsExtractor: DigitsExtractor =
+    s => {
+      val pattern = raw"(\d)".r
+      (for (m <- pattern.findAllMatchIn(s)) yield m.group(1)).toList
+    }
+
+  val numeralAndWordDigitsExtractor: DigitsExtractor =
+    s => {
+      val numerals = (1 to 9).map(_.toString)
+      val words = Seq("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+      val numeralsAndWords = numerals ++ words
+
+      val numeralsAndWordsPattern = numeralsAndWords.mkString("|")
+      val pattern = s"(?=($numeralsAndWordsPattern))".r
+      val matches = for (m <- pattern.findAllMatchIn(s)) yield m
+
+      matches
+        .toList
+        .sortBy(_.start)
+        .map(_.group(1))
+        .map(s => {
+          if (words.contains(s)) {
+            (words.indexOf(s) + 1).toString
+          }
+          else {
+            s
+          }
+        })
+    }
 
   private def parseInput(input: String): Seq[String] = {
     input.split("\n")
   }
 
-  private def extractCalibrationValues(s: String): Seq[String] = {
-    val pattern = raw"(\d)".r
-    (for (m <- pattern.findAllMatchIn(s)) yield m.group(1)).toList
+  def extractDigits(digitsExtractor: DigitsExtractor)(s: String): Seq[String] = {
+    digitsExtractor(s)
   }
 
   private def calibrationValue(xs: Seq[String]): Int = {
--- src/test/scala/adventofcode/Day01Test.scala
+++ src/test/scala/adventofcode/Day01Test.scala
@@ -6,7 +6,7 @@ import Day01._
 
 class Day01Test extends AnyFlatSpec {
 
-  private val exampleInput =
+  private val exampleInputOne =
     """1abc2
       |pqr3stu8vwx
       |a1b2c3d4e5f
@@ -15,10 +15,37 @@ class Day01Test extends AnyFlatSpec {
   behavior of "solutionPartOne"
 
   it should "return example result given example input" in {
-    solutionPartOne(exampleInput) should be(142)
+    solutionPartOne(exampleInputOne) should be(142)
   }
 
   it should "return example result given problem input" in {
     solutionPartOne(problemInput) should be(53651)
+  }
+
+  behavior of "numeralAndWordDigitsExtractor"
+
+  it should "return digits given overlaps" in {
+    val actual = extractDigits(numeralAndWordDigitsExtractor)("1twoneighthreeskipsevenineightskip1")
+    val expected = List(1, 2, 1, 8, 3, 7, 9, 8, 1).map(_.toString)
+    actual should be(expected)
+  }
+
+  behavior of "solutionPartTwo"
+
+  private val exampleInputTwo =
+    """two1nine
+      |eightwothree
+      |abcone2threexyz
+      |xtwone3four
+      |4nineeightseven2
+      |zoneight234
+      |7pqrstsixteen""".stripMargin
+
+  it should "return example result given example input" in {
+    solutionPartTwo(exampleInputTwo) should be(281)
+  }
+
+  it should "return example result given problem input" in {
+    solutionPartTwo(problemInput) should be(53894)
   }
 }
